e6ccfdc7a8d1e56426e20efa7c22110bdccb0229
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-13 22:58:58 +0100

    using both Eloquent ORM and Laravel Query Builder.

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class UsersController extends Controller
 {
     public function index() {
-        return view('users');
+        // $users = User::all();
+        $users = DB::table('users')->get();
+
+        return view('users', [ 'users' => $users ]);
     }
 }
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,1 +1,41 @@
-this is the users view.
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+  <title>Document</title>
+</head>
+<body>
+  <div>
+    @if (Auth::user())
+      hello  {{ Auth::user()->name }}      
+    @endif    
+  </div>
+  <div>
+    current users number are: {{ count($users) }}
+  </div>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">id</th>
+        <th scope="col">name</th>
+        <th scope="col">email</th>
+        <th scope="col">created_at</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach ($users as $user)
+        <tr>
+          <th scope="row">{{ $user->id }}</th>
+          <td>{{ $user->name }}</td>
+          <td>{{ $user->email }}</td>
+          {{-- @php( use Carbon\Carbon; ) --}}
+          <td>{{ Carbon\Carbon::parse($user->created_at)->diffForHumans() }}</td>
+        </tr>
+      @endforeach
+    </tbody>
+  </table>
+</body>
+</html>
