43f2fa100e189418f95f8011f5c2d2de4aeefac6
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-13 23:03:00 +0100

    added VC, waiting for the V(view).

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsersController;
+use App\Http\Controllers\CategoriesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,8 @@ Route::get('/', function () {
 });
 
 Route::middleware('admin')->get('/users', [UsersController::class, 'index']);
+
+Route::get('/categories', [CategoriesController::class, 'index']);
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
