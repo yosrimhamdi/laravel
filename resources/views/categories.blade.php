a3000840c6410c8214092d22f45e4e3c2f670721
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-19 16:53:18 +0100

    enabled bootstrap js.

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -10,7 +10,13 @@
 <body style="display: flex; padding: 4em;">
   <div class="col-md-8" style="margin-right: 1em">
     @if (session('success'))
-      <div>{{ session('success') }}</div>
+      {{-- <div class="alert alert-success" role="alert">{{ session('success') }}</div> --}}
+      <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>{{ session('success') }}</strong>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
     @endif
     <div class="card">
       <div class="card-header">All Categories</div>
@@ -56,5 +62,8 @@
       </div>
     @endif
   </div>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
