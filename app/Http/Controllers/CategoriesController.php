079bf235780c893cddb5dd6d7d5046d868fddbe7
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-19 15:22:56 +0100

    inserting new categories.

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+use Auth;
 
 class CategoriesController extends Controller
 {
@@ -11,11 +13,15 @@ class CategoriesController extends Controller
   }
 
   public function new(Request $request) {
-    $validated = $request->validate([
-      'name' => 'required'
-    ],
-    [
-     'name.required' => 'Yo dude the name is required Man!' 
+    $validated = $request->validate(
+      ['name' => 'required' ], 
+      ['name.required' => 'Yo dude the name is required Man!']
+    );
+
+    Category::insert([
+      'name' => $request->name,
+      'user_id' => Auth::user()->id,
+      'created_at' => new \DateTime()
     ]);
   }
 }
