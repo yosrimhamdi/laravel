3135b0ba7bb133007fe7d71691e98a0b238955bd
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-19 13:54:08 +0100

    using browserSync for auto realoading.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,12 +11,17 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
-        require('tailwindcss'),
-    ]);
+mix.js("resources/js/app.js", "public/js").postCss(
+    "resources/css/app.css",
+    "public/css",
+    [require("postcss-import"), require("tailwindcss")]
+);
 
 if (mix.inProduction()) {
     mix.version();
 }
+
+mix.browserSync({
+    proxy: "http://127.0.0.1:8000",
+    notify: false,
+});
