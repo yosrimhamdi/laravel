ebada9d7ba0fb1d239009bc7a6dfdf9cad222b28
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-19 14:52:25 +0100

    form validation implemented.

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -6,7 +6,16 @@ use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    public function  index() {
-        return view('categories');
-    }
+  public function  all() {
+    return view('categories');
+  }
+
+  public function new(Request $request) {
+    $validated = $request->validate([
+      'name' => 'required'
+    ],
+    [
+     'name.required' => 'Yo dude the name is required Man!' 
+    ]);
+  }
 }
--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -4,9 +4,53 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
   <title>Document</title>
 </head>
-<body>
-  hello from cat.blade.php
+<body style="display: flex; padding: 4em;">
+  <div class="col-md-8" style="margin-right: 1em">
+    <div class="card">
+      <div class="card-header">All Categories</div>
+      <div class="card-body">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="card">
+      <div class="card-header">Add New Category</div>
+      <div class="card-body">
+        <form action="/categories" method="POST">
+          @csrf
+          <div class="form-group" style="margin-bottom: 1em">
+            <label for="exampleInputEmail1">Category Name</label>
+            <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            @error('name')
+              <div class="text-danger">{{ $message }}</div>
+            @enderror
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </div>
+
+  </div>
 </body>
 </html>
