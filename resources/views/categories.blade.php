c6c8703bc867f5bf1615a54de58f5f05dbc3c5b4
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-20 21:32:16 +0100

    using the dashboard template on the categories page.

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -1,74 +1,72 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-  <title>Document</title>
-</head>
-<body style="display: flex; padding: 4em;">
-  <div class="col-md-8" style="margin-right: 1em">
-    @if (session('success'))
-      {{-- <div class="alert alert-success" role="alert">{{ session('success') }}</div> --}}
-      <div class="alert alert-warning alert-dismissible fade show" role="alert">
-        <strong>{{ session('success') }}</strong>
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-    @endif
-    <div class="card">
-      <div class="card-header">All Categories</div>
-      <div class="card-body">
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">user id</th>
-              <th scope="col">name</th>
-              <th scope="col">date</th>
-            </tr>
-          </thead>
-          <tbody>
-            @foreach ($categories as $category)
-              <tr>
-                <td>{{ $category->user->name }}</td>
-                <td>{{ $category->name }}</td>                
-                <td>
-                  @if ($category->created_at)
-                    {{ $category->created_at->diffForHumans() }}
-                  @endif
-                </td>
-              </tr>             
-            @endforeach
-          </tbody>
-        </table>
-      </div>
-      <div>{{ $categories->links() }}</div>
-    </div>
-  </div>
-  <div class="col-md-4">
-    @if (Auth::user())
-      <div class="card">
-        <div class="card-header">Add New Category</div>
-        <div class="card-body">
-          <form action="/categories" method="POST">
-            @csrf
-            <div class="form-group" style="margin-bottom: 1em">
-              <label for="exampleInputEmail1">Category Name</label>
-              <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-              @error('name')
-                <div class="text-danger">{{ $message }}</div>
-              @enderror
+<x-app-layout>
+  <x-slot name="header">
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+          {{ __('Dashboard') }}
+      </h2>
+  </x-slot>
+
+  <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg" style="display: flex; padding: 2em">
+            <div class="col-md-8" style="margin-right: 1em">
+              @if (session('success'))
+                <div class="alert alert-success" role="alert">{{ session('success') }}</div>
+                <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                  <strong>{{ session('success') }}</strong>
+                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+              @endif
+              <div class="card">
+                <div class="card-header">All Categories</div>
+                <div class="card-body">
+                  <table class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">user name</th>
+                        <th scope="col">category name</th>
+                        <th scope="col">date</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      @foreach ($categories as $category)
+                        <tr>
+                          <td>{{ $category->user->name }}</td>
+                          <td>{{ $category->name }}</td>                
+                          <td>
+                            @if ($category->created_at)
+                              {{ $category->created_at->diffForHumans() }}
+                            @endif
+                          </td>
+                        </tr>             
+                      @endforeach
+                    </tbody>
+                  </table>
+                </div>
+                <div>{{ $categories->links() }}</div>
+              </div>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </form>
-        </div>
+            <div class="col-md-4">
+              @if (Auth::user())
+                <div class="card">
+                  <div class="card-header">Add New Category</div>
+                  <div class="card-body">
+                    <form action="/categories" method="POST">
+                      @csrf
+                      <div class="form-group" style="margin-bottom: 1em">
+                        <label for="exampleInputEmail1">Category Name</label>
+                        <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                        @error('name')
+                          <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                      </div>
+                      <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                  </div>
+                </div>
+              @endif
+            </div>          </div>
       </div>
-    @endif
   </div>
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
-</html>
+</x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,16 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+        <style>
+          svg {
+            width: 20px;
+          }
+          nav {
+              padding: 1em;
+          }
+        </style>
+      
 
         @livewireStyles
 
@@ -43,4 +53,7 @@
 
         @livewireScripts
     </body>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>  
 </html>
