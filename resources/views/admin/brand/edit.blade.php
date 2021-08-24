b43254adcbad5b5a9a44f5b98a1c5efcac7f0af7
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-24 16:57:32 +0100

    [fix] wrong form action url.

--- resources/views/admin/brand/edit.blade.php
+++ resources/views/admin/brand/edit.blade.php
@@ -1,4 +1,4 @@
 <x-base title="brand">
   <img src="{{ asset($brand->image) }}" alt="brand image" style="width: 300px;">
-  <x-form action="/brands/edit/$brand->id" :brand="$brand" />
+  <x-form :action="'/brands/edit/' . $brand->id" :brand="$brand" />
 </x-base>
