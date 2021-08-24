db57f5a8ce3c21c2faf8ac6906eff38ce9675ead
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-24 19:47:50 +0100

    [fix] image extension without a dot.

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -7,21 +7,19 @@ use App\Models\Brand;
 use Illuminate\Support\Facades\File;
 
 class BrandController extends Controller {
-  private $imgPath = "images/brands/";
-
   public function index() {
     $brands = Brand::latest()->paginate(3);
 
-    return view("admin.brands.index", compact("brands"));
+    return view('admin.brands.index', compact('brands'));
   }
 
   public function newBrand(Request $request) {
     $request->validate([
-      "name" => "required",
-      "image" => "required|mimes:jpg,png,jpeg",
+      'name' => 'required',
+      'image' => 'required|mimes:jpg,png,jpeg',
     ]);
 
-    $image = $request->file("image");
+    $image = $request->file('image');
     $imagePath = $this->saveImage($image);
 
     $brand = new Brand();
@@ -29,53 +27,59 @@ class BrandController extends Controller {
     $brand->image = $imagePath;
     $brand->save();
 
-    return $this->home("success", "Brand $brand->name add successfully");
+    return $this->home('success', "Brand $brand->name add successfully");
   }
 
   public function showUpdateBrandPage($id) {
     $brand = Brand::find($id);
 
-    return view("admin.brands.edit", compact("brand", "id"));
+    return view('admin.brands.edit', compact('brand', 'id'));
   }
 
   public function updateBrand(Request $request, $id) {
-    $request->validate(["name" => "required"]);
+    $request->validate(['name' => 'required']);
 
     $brand = Brand::find($id);
-    $image = $request->file("image");
+    $image = $request->file('image');
     $updates = [];
 
     if ($image) {
       File::delete($brand->image);
 
-      $updates["image"] = $this->saveImage($image);
+      $updates['image'] = $this->saveImage($image);
     } else {
-      $updates["name"] = $request->name;
+      $updates['name'] = $request->name;
     }
 
     $brand->update($updates);
 
-    return $this->home("brand updated successfully");
+    return $this->home('brand updated successfully');
   }
 
   public function deleteBrand($id) {
-    Brand::find($id)->delete();
+    $brand = Brand::find($id);
 
-    return $this->home("brand deleted successfully");
+    File::delete($brand->image);
+
+    $brand->delete();
+
+    return $this->home('brand deleted successfully');
   }
 
   private function home($message = null) {
     return Redirect()
-      ->to("/brands")
-      ->with("success", $message);
+      ->to('/brands')
+      ->with('success', $message);
   }
 
   private function saveImage($image) {
     $id = hexdec(uniqid());
     $ext = strtolower($image->getClientOriginalExtension());
+    $fileName = $id . '.' . $ext;
+    $imageDir = 'images/brands/';
 
-    $image->move($this->imgPath, $id . $ext);
+    $image->move($imageDir, $fileName);
 
-    return $this->imgPath . $id . $ext;
+    return $imageDir . $fileName;
   }
 }
