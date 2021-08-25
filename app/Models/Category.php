44befbed1cdd950eec430d3701860f6fda1e5635
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-25 10:16:07 +0100

    prettier format.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,16 +6,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Category extends Model
-{
-    use SoftDeletes;
+class Category extends Model {
+  use SoftDeletes;
 
-    protected $fillable = [
-        'name',
-        'user_id'
-    ];
+  protected $fillable = ['name', 'user_id'];
 
-    public function user() {
-        return $this->hasOne(User::class, 'id', 'user_id');
-    }
+  public function user() {
+    return $this->hasOne(User::class, 'id', 'user_id');
+  }
 }
