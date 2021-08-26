0525f6439901cc42c73649cc24104a5f7cbdd9f8
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-26 11:18:34 +0100

    protecting the categories route.

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -7,20 +7,26 @@ use App\Models\Category;
 use Illuminate\Support\Facades\DB;
 use Auth;
 
-class CategoriesController extends Controller
-{
-  public function  all() {
+class CategoriesController extends Controller {
+  public function __construct() {
+    $this->middleware('auth');
+  }
+
+  public function all() {
     $categories = Category::latest()->paginate(5);
 
     $trashedCategories = Category::onlyTrashed()->get();
-    
-    return view('categories.index', compact('categories', 'trashedCategories'));
+
+    return view(
+      'admin.categories.index',
+      compact('categories', 'trashedCategories')
+    );
   }
 
   public function new(Request $request) {
     $this->validateRequest($request);
-    
-    $category = new Category;    
+
+    $category = new Category();
     $category->name = $request->name;
     $category->user_id = Auth::user()->id;
     $category->save();
@@ -50,23 +56,29 @@ class CategoriesController extends Controller
 
   private function validateRequest(Request $request) {
     $validated = $request->validate(
-      ['name' => 'required' ], 
+      ['name' => 'required'],
       ['name.required' => 'Yo dude the name is required Man!']
     );
   }
-  
+
   private function goHome($message) {
-    return Redirect()->to('/categories')->with('success', $message);
+    return Redirect()
+      ->to('/categories')
+      ->with('success', $message);
   }
 
   public function restore($id) {
-    Category::withTrashed()->find($id)->restore();
+    Category::withTrashed()
+      ->find($id)
+      ->restore();
 
     return $this->goHome('restored');
   }
 
   public function permDelete($id) {
-    Category::onlyTrashed()->find($id)->forceDelete();
+    Category::onlyTrashed()
+      ->find($id)
+      ->forceDelete();
 
     return $this->goHome('got rid of it.');
   }
