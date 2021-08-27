1e1ef3c787e1f87850b69c2746f9e8d9e74d8692
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-27 19:36:00 +0100

    formatter.

--- resources/views/components/base.blade.php
+++ resources/views/components/base.blade.php
@@ -1,15 +1,18 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ $title }}
-        </h2>
-    </x-slot>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+      {{ $title }}
+    </h2>
+  </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg" style="padding: 1em;">
-                {{ $slot }}
-            </div>
-        </div>
+  <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div
+        class="bg-white overflow-hidden shadow-xl sm:rounded-lg"
+        style="padding: 1em;"
+      >
+        {{ $slot }}
+      </div>
     </div>
+  </div>
 </x-app-layout>
