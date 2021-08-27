5ced358a5f0c644aa6f02b7660e8f33568b31b7d
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-27 18:47:05 +0100

    loggin out the user.

--- resources/views/components/admin/header.blade.php
+++ resources/views/components/admin/header.blade.php
@@ -1,17 +1,35 @@
-<header class="main-header " id="header">
+<header
+  class="main-header "
+  id="header"
+>
   <nav class="navbar navbar-static-top navbar-expand-lg">
     <!-- Sidebar toggle button -->
-    <button id="sidebar-toggler" class="sidebar-toggle">
+    <button
+      id="sidebar-toggler"
+      class="sidebar-toggle"
+    >
       <span class="sr-only">Toggle navigation</span>
     </button>
     <!-- search form -->
     <div class="search-form d-none d-lg-inline-block">
       <div class="input-group">
-        <button type="button" name="search" id="search-btn" class="btn btn-flat">
+        <button
+          type="button"
+          name="search"
+          id="search-btn"
+          class="btn btn-flat"
+        >
           <i class="mdi mdi-magnify"></i>
         </button>
-        <input type="text" name="query" id="search-input" class="form-control" placeholder="'button', 'chart' etc."
-          autofocus autocomplete="off" />
+        <input
+          type="text"
+          name="query"
+          id="search-input"
+          class="form-control"
+          placeholder="'button', 'chart' etc."
+          autofocus
+          autocomplete="off"
+        />
       </div>
       <div id="search-results-container">
         <ul id="search-results"></ul>
@@ -22,15 +40,21 @@
       <ul class="nav navbar-nav">
         <!-- Github Link Button -->
         <li class="github-link mr-3">
-          <a class="btn btn-outline-secondary btn-sm" href="https://github.com/tafcoder/sleek-dashboard"
-            target="_blank">
+          <a
+            class="btn btn-outline-secondary btn-sm"
+            href="https://github.com/tafcoder/sleek-dashboard"
+            target="_blank"
+          >
             <span class="d-none d-md-inline-block mr-2">Source Code</span>
             <i class="mdi mdi-github-circle"></i>
           </a>
 
         </li>
         <li class="dropdown notifications-menu">
-          <button class="dropdown-toggle" data-toggle="dropdown">
+          <button
+            class="dropdown-toggle"
+            data-toggle="dropdown"
+          >
             <i class="mdi mdi-bell-outline"></i>
           </button>
           <ul class="dropdown-menu dropdown-menu-right">
@@ -71,20 +95,35 @@
               </a>
             </li>
             <li class="dropdown-footer">
-              <a class="text-center" href="#"> View All </a>
+              <a
+                class="text-center"
+                href="#"
+              > View All </a>
             </li>
           </ul>
         </li>
         <!-- User Account -->
         <li class="dropdown user-menu">
-          <button href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
-            <img src="{{ asset('admin/img/user/user.png') }}" class="user-image" alt="User Image" />
+          <button
+            href="#"
+            class="dropdown-toggle nav-link"
+            data-toggle="dropdown"
+          >
+            <img
+              src="{{ asset('admin/img/user/user.png') }}"
+              class="user-image"
+              alt="User Image"
+            />
             <span class="d-none d-lg-inline-block">Abdus Salam</span>
           </button>
           <ul class="dropdown-menu dropdown-menu-right">
             <!-- User image -->
             <li class="dropdown-header">
-              <img src="{{ asset('admin/img/user/user.png') }}" class="img-circle" alt="User Image" />
+              <img
+                src="{{ asset('admin/img/user/user.png') }}"
+                class="img-circle"
+                alt="User Image"
+              />
               <div class="d-inline-block">
                 Abdus Salam <small class="pt-1">abdus@gmail.com</small>
               </div>
@@ -108,7 +147,13 @@
             </li>
 
             <li class="dropdown-footer">
-              <a href="signin.html"> <i class="mdi mdi-logout"></i> Log Out </a>
+              <form
+                action="/logout"
+                method="POST"
+              >
+                @csrf
+                <button type="submit"><a><i class="mdi mdi-logout"></i> Log Out</a></button>
+              </form>
             </li>
           </ul>
         </li>
