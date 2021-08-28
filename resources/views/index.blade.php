221efd2b887fe22af3299f7da66036bfd037ba21
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-28 10:15:48 +0100

    Removed extra nav links.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -99,35 +99,11 @@
 
       <nav class="nav-menu d-none d-lg-block">
         <ul>
-          <li class="active"><a href="index.html">Home</a></li>
-
-          <li class="drop-down"><a href="">About</a>
-            <ul>
-              <li><a href="about.html">About Us</a></li>
-              <li><a href="team.html">Team</a></li>
-              <li><a href="testimonials.html">Testimonials</a></li>
-              <li class="drop-down"><a href="#">Deep Drop Down</a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-
-          <li><a href="services.html">Services</a></li>
-          <li><a href="portfolio.html">Portfolio</a></li>
-          <li><a href="pricing.html">Pricing</a></li>
-          <li><a href="blog.html">Blog</a></li>
-          <li><a href="contact.html">Contact</a></li>
           @if (Auth::user())
             <li><a href="{{ url('/dashboard') }}">Dashboard</a></li>
           @else
             <li><a href="{{ url('/login') }}">Log In</a></li>
-            <li><a href="{{ url('/register') }}">Log In</a></li>
+            <li><a href="{{ url('/register') }}">Register</a></li>
           @endif
         </ul>
       </nav><!-- .nav-menu -->
