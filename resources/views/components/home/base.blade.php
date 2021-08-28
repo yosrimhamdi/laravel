09adcae140910a6558046b49059b12d73a3dd8a5
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-28 10:40:18 +0100

    Devided the home page into seperate componenet.

---
+++ resources/views/components/home/base.blade.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta
+    content="width=device-width, initial-scale=1.0"
+    name="viewport"
+  >
+
+  <title>Company Bootstrap Template - Index</title>
+  <meta
+    content=""
+    name="descriptison"
+  >
+  <meta
+    content=""
+    name="keywords"
+  >
+
+  {{-- href="{{ asset('front/') }}" --}}
+
+  <!-- Favicons -->
+  <link
+    href="{{ asset('front/img/favicon.png') }}"
+    rel="icon"
+  >
+  <link
+    href="{{ asset('front/img/apple-touch-icon.png') }}"
+    rel="apple-touch-icon"
+  >
+
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet"
+  >
+
+  <!-- Vendor CSS Files -->
+  <link
+    href="{{ asset('front/vendor/bootstrap/css/bootstrap.min.css') }}"
+    rel="stylesheet"
+  >
+  <link
+    href="{{ asset('front/vendor/icofont/icofont.min.css') }}"
+    rel="stylesheet"
+  >
+  <link
+    href="{{ asset('front/vendor/boxicons/css/boxicons.min.css') }}"
+    rel="stylesheet"
+  >
+  <link
+    href="{{ asset('front/vendor/animate.css/animate.min.css') }}"
+    rel="stylesheet"
+  >
+  <link
+    href="{{ asset('front/vendor/venobox/venobox.css') }}"
+    rel="stylesheet"
+  >
+  <link
+    href="{{ asset('front/vendor/owl.carousel/assets/owl.carousel.min.css') }}"
+    rel="stylesheet"
+  >
+  <link
+    href="{{ asset('front/vendor/aos/aos.css') }}"
+    rel="stylesheet"
+  >
+  <link
+    href="{{ asset('front/vendor/remixicon/remixicon.css') }}"
+    rel="stylesheet"
+  >
+
+  <!-- Template Main CSS File -->
+  <link
+    href="{{ asset('front/css/style.css') }}"
+    rel="stylesheet"
+  >
+</head>
+
+<body>
+
+  <x-home.header />
+  <div class="content"> {{ $slot }} </div>
+  <x-home.footer />
+
+  <a
+    href="#"
+    class="back-to-top"
+  ><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="{{ asset('front/vendor/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('front/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('front/vendor/jquery.easing/jquery.easing.min.js') }}"></script>
+  <script src="{{ asset('front/vendor/php-email-form/validate.js') }}"></script>
+  <script src="{{ asset('front/vendor/jquery-sticky/jquery.sticky.js') }}"></script>
+  <script src="{{ asset('front/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+  <script src="{{ asset('front/vendor/venobox/venobox.min.js') }}"></script>
+  <script src="{{ asset('front/vendor/waypoints/jquery.waypoints.min.js') }}"></script>
+  <script src="{{ asset('front/vendor/owl.carousel/owl.carousel.min.js') }}"></script>
+  <script src="{{ asset('front/vendor/aos/aos.js') }}"></script>
+
+  <!-- Template Main JS File -->
+  <script src="{{ asset('front/js/main.js') }}"></script>
+
+</body>
+
+</html>
---
+++ resources/views/components/home/header.blade.php
@@ -0,0 +1,42 @@
+<header
+  id="header"
+  class="fixed-top"
+>
+  <div class="container d-flex align-items-center">
+
+    <h1 class="logo mr-auto"><a href="index.html"><span>Com</span>pany</a></h1>
+    <!-- Uncomment below if you prefer to use an image logo -->
+    <!-- <a href="index.html" class="logo mr-auto"><img src="{{ asset('front/img/logo.png') }}" alt="" class="img-fluid"></a>-->
+
+    <nav class="nav-menu d-none d-lg-block">
+      <ul>
+        @if (Auth::user())
+          <li><a href="{{ url('/dashboard') }}">Dashboard</a></li>
+        @else
+          <li><a href="{{ url('/login') }}">Log In</a></li>
+          <li><a href="{{ url('/register') }}">Register</a></li>
+        @endif
+      </ul>
+    </nav><!-- .nav-menu -->
+
+    <div class="header-social-links">
+      <a
+        href="#"
+        class="twitter"
+      ><i class="icofont-twitter"></i></a>
+      <a
+        href="#"
+        class="facebook"
+      ><i class="icofont-facebook"></i></a>
+      <a
+        href="#"
+        class="instagram"
+      ><i class="icofont-instagram"></i></a>
+      <a
+        href="#"
+        class="linkedin"
+      ><i class="icofont-linkedin"></i></i></a>
+    </div>
+
+  </div>
+</header>
