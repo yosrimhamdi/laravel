88ebe940a944c8e0699d1fdfd5d85bfffa3fc350
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-29 14:16:02 +0100

    rendreing carousel images.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Brand;
+use App\Models\Slider;
 
 class HomeController extends Controller {
   public function index() {
     $brands = Brand::all();
+    $slides = Slider::all();
 
-    return view('index', compact('brands'));
+    return view('index', compact(['brands', 'slides']));
   }
 }
--- resources/views/components/home/carousel.blade.php
+++ resources/views/components/home/carousel.blade.php
@@ -4,71 +4,28 @@
     class="carousel slide carousel-fade"
     data-ride="carousel"
   >
-
     <div
       class="carousel-inner"
       role="listbox"
     >
-
-      <!-- Slide 1 -->
-      <div
-        class="carousel-item active"
-        style="background-image: url('{{ asset('front/img/slide/slide-1.jpg') }}')"
-      >
-        <div class="carousel-container">
-          <div class="carousel-content animate__animated animate__fadeInUp">
-            <h2>Welcome to <span>Company</span></h2>
-            <p>Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam.
-              Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus
-              deleniti vel. Minus et tempore modi architecto.</p>
-            <div class="text-center"><a
-                href=""
-                class="btn-get-started"
-              >Read More</a></div>
+      @foreach ($slides as $slide)
+        <div
+          class="carousel-item active"
+          style="background-image: url('{{ asset($slide->image) }}')"
+        >
+          <div class="carousel-container">
+            <div class="carousel-content animate__animated animate__fadeInUp">
+              <h2>{{ $slide->title }}</h2>
+              <p>{{ $slide->description }}</p>
+              <div class="text-center"><a
+                  href=""
+                  class="btn-get-started"
+                >Read More</a></div>
+            </div>
           </div>
         </div>
-      </div>
-
-      <!-- Slide 2 -->
-      <div
-        class="carousel-item"
-        style="background-image: url('{{ asset('front/img/slide/slide-2.jpg') }}')"
-      >
-        <div class="carousel-container">
-          <div class="carousel-content animate__animated animate__fadeInUp">
-            <h2>Lorem Ipsum Dolor</h2>
-            <p>Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam.
-              Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus
-              deleniti vel. Minus et tempore modi architecto.</p>
-            <div class="text-center"><a
-                href=""
-                class="btn-get-started"
-              >Read More</a></div>
-          </div>
-        </div>
-      </div>
-
-      <!-- Slide 3 -->
-      <div
-        class="carousel-item"
-        style="background-image: url('{{ asset('front/img/slide/slide-3.jpg') }}')"
-      >
-        <div class="carousel-container">
-          <div class="carousel-content animate__animated animate__fadeInUp">
-            <h2>Sequi ea ut et est quaerat</h2>
-            <p>Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam.
-              Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus
-              deleniti vel. Minus et tempore modi architecto.</p>
-            <div class="text-center"><a
-                href=""
-                class="btn-get-started"
-              >Read More</a></div>
-          </div>
-        </div>
-      </div>
-
+      @endforeach
     </div>
-
     <a
       class="carousel-control-prev"
       href="#heroCarousel"
@@ -81,7 +38,6 @@
       ></span>
       <span class="sr-only">Previous</span>
     </a>
-
     <a
       class="carousel-control-next"
       href="#heroCarousel"
@@ -94,11 +50,9 @@
       ></span>
       <span class="sr-only">Next</span>
     </a>
-
     <ol
       class="carousel-indicators"
       id="hero-carousel-indicators"
     ></ol>
-
   </div>
 </section>
