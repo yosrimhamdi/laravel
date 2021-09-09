dd61e0b301c7c4f1859213b6fcc597877689d650
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-09 17:29:29 +0100

    removed progress bar init.

--- resources/views/components/admin/base.blade.php
+++ resources/views/components/admin/base.blade.php
@@ -102,12 +102,6 @@
   class="sidebar-fixed sidebar-dark header-light header-fixed"
   id="body"
 >
-  <script>
-    NProgress.configure({
-      showSpinner: false
-    });
-    NProgress.start();
-  </script>
   <div class="mobile-sticky-body-overlay"></div>
   <div class="wrapper">
     <x-admin.side-bar />
